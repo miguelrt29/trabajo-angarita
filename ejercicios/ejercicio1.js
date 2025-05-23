4f2f0b14ef06228d4ad5c043a91f4007f1b34ae9
miguelrt29 <miguelrt2903@gmail.com>
2025-05-23 05:51:56 +0200

    AGREGADO primer ejercicio

---
+++ ejercicios/ejercicio1.js
@@ -0,0 +1,28 @@
+const archivos = require ('fs/promises');
+
+async function crearArchivos (){
+    for(let i = 1; i <=10; i++){
+
+        const nombreArchivo = `archivo_${i}.txt`;
+        const contenido = `este es el numero ${i}`;
+
+        await archivos.writeFile(nombreArchivo,contenido);
+
+        console.log(`archivo ${i} creado`);
+        
+    }
+
+    console.log("todos los archivos han sido creados.");
+    
+}
+
+crearArchivos()
+.then(()=>{
+    console.log("pasos completados");
+    
+})
+
+.catch (()=>{
+    console.log("ocurrio un error: ", error);
+    
+})
