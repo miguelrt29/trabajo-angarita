67e27407278cec2e20da3bf3dd95104364da4a5c
miguelrt29 <miguelrt2903@gmail.com>
2025-05-23 12:35:36 -0500

    AGREGADO cambio de codigo

--- ejercicios/ejercicio3.js
+++ ejercicios/ejercicio3.js
@@ -1,44 +1,19 @@
 const fetch = require('node-fetch');
 
-async function fetchJsonSafe(url) {
-  try {
-    const res = await fetch(url);
-    const text = await res.text();
-    try {
-      return JSON.parse(text);
-    } catch {
-      // No es JSON válido, devolver null para manejar después
-      return null;
-    }
-  } catch {
-    return null;
-  }
-}
-
 async function obtenerClima() {
-  const api1 = fetchJsonSafe('https://api.open-meteo.com/v1/forecast?latitude=6.25184&longitude=-75.56359&current_weather=truehttps://api.met.no/weatherapi/locationforecast/2.0/compact?lat=6.25184&lon=-75.56359')
-    .then(data => data ? `Open-Meteo: ${data.current_weather.temperature}°C` : null);
+  const api1 = fetch('https://api.open-meteo.com/v1/forecast?latitude=6.25184&longitude=-75.56359&current_weather=true')
+    .then(res => res.json())
+    .then(data => `Open-Meteo: ${data.current_weather.temperature}°C`)
+    .catch(() => Promise.reject());
 
-  const api2 = fetchJsonSafe('https://wttr.in/Medellin?format=j1')
-    .then(data => data ? `Wttr.in: ${data.current_condition[0].temp_C}°C` : null);
+  const api2 = fetch('https://goweather.herokuapp.com/weather/Medellin')
+    .then(res => res.json())
+    .then(data => `GoWeather: ${data.temperature}`)
+    .catch(() => Promise.reject());
 
-
-
-  try {
-    // Promise.race no puede ignorar valores null, filtramos resultados no nulos
-    const resultado = await Promise.race([
-      api1.catch(() => null),
-      api2.catch(() => null),
-    ]);
-
-    if (resultado) {
-      console.log(`Temperatura más rápida recibida: ${resultado}`);
-    } else {
-      console.log('No se pudo obtener la temperatura de ninguna API');
-    }
-  } catch (err) {
-    console.log("Error al obtener el clima:", err);
-  }
+  Promise.race([api1, api2])
+    .then(console.log)
+    .catch(() => console.log('No se pudo obtener la temperatura'));
 }
- 
+
 obtenerClima();
