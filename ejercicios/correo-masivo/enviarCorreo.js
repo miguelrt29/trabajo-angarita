5575d3aa08c4c2880141a90978af97dc8bd76dd9
miguelrt29 <miguelrt2903@gmail.com>
2025-05-23 14:18:06 -0500

    agrego

--- ejercicios/correo-masivo/enviarCorreo.js
+++ ejercicios/correo-masivo/enviarCorreo.js
@@ -1,38 +1,39 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const destinatarios = [
-    'miguelrt29@ejemplo.com',
-    'miguelrt@ejemplo.com',
-    'miguelrt29@ejemplo.com',
+  "miguelrt29@ejemplo.com",
+  "miguelrt@ejemplo.com",
+  "miguelrt29@ejemplo.com",
 ];
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'Pepitooperez148501@gmail.com',
-        pass: 'Hacker1041.'
-    }
+  service: "gmail",
+  auth: {
+    user: "Pepitooperez148501@gmail.com",
+    pass: "Hacker1041.",
+  },
 });
 
 async function enviarCorreo(destinatario) {
-    try {
-        await transporter.sendMail({
-            from: '"Tu Nombre" miguelrt29@gmail.com',
-            to: destinatario,
-            subject: 'Correo masivo de prueba',
-            text: '¡Hola! Este es un mensaje de prueba enviado con Nodemailer.',
-        });
+  try {
+    await transporter.sendMail({
+      from: '"Tu Nombre" miguelrt29@gmail.com',
+      to: destinatario,
+      subject: "Correo masivo de prueba",
+      text: "¡Hola! Este es un mensaje de prueba enviado con Nodemailer.",
+    });
 
-        console.log(` Correo enviado a ${destinatario}`);
-    } catch (error) {
-        console.error(` Error con ${destinatario}:`, error.message);
-    }
+    console.log(` Correo enviado a ${destinatario}`);
+  } catch (error) {
+    console.error(` Error con ${destinatario}:`, error.message);
+  }
 }
 
 async function enviarCorreosMasivos() {
-    const tareas = destinatarios.map(destino => enviarCorreo(destino));
-    await Promise.all(tareas); 
-    console.log(' Todos los correos han sido procesados.y se mostraran enseguida');
+  const tareas = destinatarios.map((destino) => enviarCorreo(destino));
+  await Promise.all(tareas);
+  console.log(
+    " Todos los correos han sido procesados.y se mostraran enseguida"
+  );
 }
-
 
 enviarCorreosMasivos();
